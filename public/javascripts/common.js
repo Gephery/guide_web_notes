19338f1168c0097e05ab76d3931848353d2c1a62
Gephery <grucemax@uw.edu>
2016-12-24 05:53:10 -0500

    Initial commit with express' build structure

---
+++ public/javascripts/common.js
@@ -0,0 +1,11 @@
+/**
+ * Created by gephery on 12/24/16.
+ */
+(function() {
+    "use strict";
+
+    function checkLogin(redirectToLogin, redirectToHome) {
+
+    }
+
+})();
---
+++ public/javascripts/getServerUtil.js
@@ -0,0 +1,6 @@
+/**
+ * Created by gephery on 12/23/16.
+ */
+(function() {
+    "use strict";
+});
